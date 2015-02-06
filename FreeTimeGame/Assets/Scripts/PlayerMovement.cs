f7e35a93887f958d1bc7da9cbab9a2bdae361ef8
downinew <downinew@rose-hulman.edu>
2015-02-05 23:33:14 -0500

    finished modifying the terrain for now, working on the character prefab, need to implement jump

---
+++ FreeTimeGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    private CharacterController playerController;
+    public float playerSpeed = 8.0f;
+    public float gravity = 9.81f;
+    public float playerJumpStrength = 50.0f;
+
+	// Use this for initialization
+	void Start () {
+        playerController = GetComponent<CharacterController>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        playerController.Move(Vector3.forward * Input.GetAxis("Vertical") * Time.deltaTime * playerSpeed);
+        playerController.Move(Vector3.right * Input.GetAxis("Horizontal") * Time.deltaTime * playerSpeed);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            
+        }
+        playerController.Move(Vector3.down * gravity * Time.deltaTime);
+
+        
+	}
+}
