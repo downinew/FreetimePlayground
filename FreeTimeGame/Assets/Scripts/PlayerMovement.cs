4da5bb3e65e1659cb720d7404f0b660e52867c69
downinew <downinew@rose-hulman.edu>
2015-02-09 03:09:29 -0500

    adding new code for the player

--- FreeTimeGame/Assets/Scripts/PlayerMovement.cs
+++ FreeTimeGame/Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,8 @@ public class PlayerMovement : MonoBehaviour {
     public float playerSpeed = 8.0f;
     public float gravity = 9.81f;
     public float playerJumpStrength = 50.0f;
+	private float horizontalRotation = 0f;
+	public float mouseSensitivity = 15.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -17,6 +19,9 @@ public class PlayerMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+		horizontalRotation = Input.GetAxis("Mouse X") * mouseSensitivity;
+
+
         playerController.Move(Vector3.forward * Input.GetAxis("Vertical") * Time.deltaTime * playerSpeed);
         playerController.Move(Vector3.right * Input.GetAxis("Horizontal") * Time.deltaTime * playerSpeed);
         if (Input.GetKeyDown(KeyCode.Space))
@@ -24,7 +29,7 @@ public class PlayerMovement : MonoBehaviour {
             
         }
         playerController.Move(Vector3.down * gravity * Time.deltaTime);
-
+		playerController.transform.localRotation = Quaternion.Euler(0,horizontalRotation,0);
         
 	}
 }
