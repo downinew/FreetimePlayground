5ea4b720a06f268987b474d145e1c36d20b30dd1
downinew <downinew@rose-hulman.edu>
2015-02-12 21:23:07 -0500

    Character is now moving properly, still needs tweaking but is fine for now

---
+++ FreeTimeGame/Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRotation : MonoBehaviour {
+
+    //Public for access
+    public float mouseSensitivity = 15.0f;
+
+    //Private for exclusive
+    private Camera playerCamera;
+    private float verticalRotation = 0f;
+    private float topNeckRange = 75f;
+    private float bottomNeckRange = -80f;
+
+
+
+	// Use this for initialization
+	void Start () {
+        playerCamera = GetComponentInChildren<Camera>();
+	}
+	
+	// Update the Camera
+	void Update () {
+        verticalRotation -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+        verticalRotation = Mathf.Clamp(verticalRotation, bottomNeckRange, topNeckRange);
+        playerCamera.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
+
+	}
+}
--- FreeTimeGame/Assets/Scripts/PlayerMovement.cs
+++ FreeTimeGame/Assets/Scripts/PlayerMovement.cs
@@ -3,33 +3,54 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 
+    //Public to be changed 
+    public float mouseSensitivity = 15.0f;
+    public float playerSpeed = 50.0f;
+    public float playerJumpStrength = 10.0f;
+
+    //Private to keep the same
     private CharacterController playerController;
-    public float playerSpeed = 8.0f;
-    public float gravity = 9.81f;
-    public float playerJumpStrength = 50.0f;
 	private float horizontalRotation = 0f;
-	public float mouseSensitivity = 15.0f;
+    private Transform playerTransform;
+    private float forwardSpeed;
+    private float sideSpeed;
+    private Vector3 speed;
+    private float fallingSpeed = 0f;
 
 	// Use this for initialization
 	void Start () {
         playerController = GetComponent<CharacterController>();
+        playerTransform = GetComponent<Transform>();
+        Screen.lockCursor = true;
 
     }
 	
 	// Update is called once per frame
 	void Update () {
-
-		horizontalRotation = Input.GetAxis("Mouse X") * mouseSensitivity;
-
-
-        playerController.Move(Vector3.forward * Input.GetAxis("Vertical") * Time.deltaTime * playerSpeed);
-        playerController.Move(Vector3.right * Input.GetAxis("Horizontal") * Time.deltaTime * playerSpeed);
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
-            
+            Application.Quit();
         }
-        playerController.Move(Vector3.down * gravity * Time.deltaTime);
-		playerController.transform.localRotation = Quaternion.Euler(0,horizontalRotation,0);
+
+
+        //Rotation of the player
+		horizontalRotation = Input.GetAxis("Mouse X") * mouseSensitivity;
+        playerTransform.Rotate(0, horizontalRotation, 0);
+
+
+        //Speed and movement of Character
+        forwardSpeed = Input.GetAxis("Vertical") * playerSpeed;
+        sideSpeed = Input.GetAxis("Horizontal") * playerSpeed;
+        fallingSpeed += Physics.gravity.y * Time.deltaTime;
+        if (Input.GetButtonDown("Jump") && playerController.isGrounded)
+        {
+            fallingSpeed = playerJumpStrength;
+        }
+        speed = new Vector3(sideSpeed, fallingSpeed, forwardSpeed);
+        speed = playerTransform.rotation * speed;
+        playerController.Move(speed * Time.deltaTime);
+
+  
         
 	}
 }
